a07332127c26365ae1dec6fe7742170c3fd9c6a1
peuh <savoiepe@gmail.com>
2022-09-19 15:43:07 -0400

    readMe and requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # churn_prediction
+
+This is a simple churn prediction app made using the dataset from an old kaggle competition : https://www.kaggle.com/competitions/customer-churn-prediction-2020/data 
+
+To run, make sure to have all dependancies installed or install them using `pip install requirements.txt`
+
+Then launch the app with `streamlit run app.py`
+
+This short project is mostly inspired by the following video from Nicholas Renotte on Youtube : https://www.youtube.com/watch?v=Ebb4gUI2IpQ&ab_channel=NicholasRenotte 
--- app.py
+++ app.py
@@ -64,7 +64,7 @@ with st.form("form key", clear_on_submit=True):
         return df
 
     def predict():
-        x = np.array([state, account_length, "area_code"+str(area_code), internationnal_plan, voice_mail_plan, number_vmail_messages,
+        x = np.array([state, account_length, "area_code_"+str(area_code), internationnal_plan, voice_mail_plan, number_vmail_messages,
         total_day_minutes, total_day_calls, total_day_charge, total_eve_minutes, total_eve_calls, total_eve_charge, total_night_minutes,
         total_night_calls, total_night_charge, total_intl_minutes, total_intl_calls, total_intl_charge, number_customer_service_calls])
 
